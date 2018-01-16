171ee9e66054b9fed0846c089fe5230ea4bcea6a
renanbronchart <renan.bronchart@gmail.com>
2018-01-16 02:04:49 +0100

    [Feat:map] In development

--- src/components/d3-components/map.vue
+++ src/components/d3-components/map.vue
@@ -30,7 +30,9 @@ export default {
       rootWidth: false,
       previousWidth: false,
       path: false,
-      heat: false
+      heat: false,
+      mapLayer: false,
+      heatLayer: false
     }
   },
   mounted () {
@@ -48,23 +50,17 @@ export default {
     var svg = d3.select(this.map.getPanes().overlayPane).append('svg')
 
     // this.svg = svg
-    // console.log(this.svg, 'this.svg')
     var transform = d3.geoTransform({ point: this.projectPoint })
     self.path = d3.geoPath().projection(transform)
 
     function makeGraphs (error, recordsJson) {
-      console.log(recordsJson)
       if (error) {
         console.log('error')
       }
       // Clean data
       var records = [...recordsJson]
-
       // Create a Crossfilter instance
       var ndx = crossfilter(records)
-
-      console.log(ndx, 'ndx')
-      console.log(records, 'records')
 
       // Define Dimensions
       var populationDim = ndx.dimension(function (d) {
@@ -78,15 +74,10 @@ export default {
       var departementGroup = departementDim.group()
       var all = ndx.groupAll()
 
-      // console.log(departementGroup.groupAll().value(), 'populationDim')
-
       // Charts
       var numberRecordsND = dc.numberDisplay('#number-records-nd')
       var populationBrandChart = dc.rowChart('#population-chart')
       var departementChart = dc.rowChart('#departement-row-chart')
-
-      console.log(populationBrandChart, 'populationBrandChart')
-      console.log(departementChart, 'departementChart')
 
       numberRecordsND
         .formatNumber(d3.format('d'))
@@ -106,8 +97,8 @@ export default {
         .ticks(4)
 
       populationBrandChart
-        .width(500)
-        .height(10000)
+        .width(600)
+        .height(300)
         .dimension(populationDim)
         .group(populationGroup)
         .colors(['#6baed6'])
@@ -116,30 +107,25 @@ export default {
         .xAxis()
         .ticks(10)
 
-      // console.log(populationBrandChart, 'populationBrandChart')
-      // console.log(departementChart, 'departementChart')
-
       // Draw Map
-      self.drawMap(svg, allDim)
+      self.drawMap(svg, allDim, true)
 
       // Update the heatmap if any dc chart get filtered
       var dcCharts = [populationBrandChart, departementChart]
 
       dcCharts.forEach(function (dcChart) {
-        console.log(dcChart, 'dcChart')
-
         // stop here
         dcChart.on('filtered', function (chart, filter) {
           var transitionPoints = new Promise(
             // La fonction de résolution est appelée avec la capacité de
             // tenir ou de rompre la promesse
             function (resolve, reject) {
-              d3.select('.leaflet-container')
+              d3.selectAll('.leaflet-heatmap-layer')
                 .transition()
                 .duration(700)
                 .style('opacity', '0')
                 .transition()
-                .duration(500)
+                .duration(600)
                 .style('opacity', '1')
 
               setTimeout(function () {
@@ -151,10 +137,7 @@ export default {
           transitionPoints.then(
             // On affiche un message avec la valeur
             function () {
-              self.map.eachLayer(function (layer) {
-                self.map.removeLayer(layer)
-              })
-
+              self.map.removeLayer(self.heatLayer)
               self.drawMap(svg, allDim)
             })
             .catch(
@@ -169,22 +152,20 @@ export default {
     }
   },
   methods: {
-    drawMap (svg, allDim) {
+    drawMap (svg, allDim, mapConstruct) {
       // var commercesGroup = svg.append('g').attr('class', 'leaflet-zoom-hide')
       // map.setView([31.75, 110], 4)
-      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        attribution: 'Map data &copy <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-        maxZoom: 30,
-        minZoom: 8,
-        id: 'mapbox.streets',
-        accessToken: 'sk.eyJ1IjoicmVuYW5icm9uY2hhcnQiLCJhIjoiY2o5OW84enp2MHoyOTMzbndla3cyN3hrcCJ9.DdB659aMvccAu0B6ag8aJA'
-      }).addTo(this.map)
+      if (mapConstruct) {
+        this.mapLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+          attribution: 'Map data &copy <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+          maxZoom: 30,
+          minZoom: 8,
+          id: 'mapbox.streets',
+          accessToken: 'sk.eyJ1IjoicmVuYW5icm9uY2hhcnQiLCJhIjoiY2o5OW84enp2MHoyOTMzbndla3cyN3hrcCJ9.DdB659aMvccAu0B6ag8aJA'
+        }).addTo(this.map)
+      }
 
-      var allDimTop = allDim.top(Infinity)
-      // var radius = d3.scaleLinear()
-      //   .domain([0, d3.max(allDimTop, function (d) { return +d.fields.population })])
-      //   .range([2, 50])
-
+      const allDimTop = allDim.top(Infinity)
       const maxValue = d3.max(allDimTop, function (d) { return +d.fields.population })
 
       // HeatMap
@@ -195,31 +176,27 @@ export default {
         geoData.push([d.fields.geo_point_2d[0], d.fields.geo_point_2d[1], ((d.fields.population) / maxValue)])
       })
 
-      // this.featureCommerces = commercesGroup.selectAll('.commerces')
-      //   .data(allDimTop)
-      //   .enter()
-      //   .append('path')
-      //   .attr('class', 'commerces')
-      //   .attr('d', this.path.pointRadius(function (d) {
-      //     return radius(Math.floor((d.fields.population)) * 100)
-      //   }))
-      //   .style('fill', 'black')
-      //   .style('z-index', function (d) { return Math.floor((d.fields.population)) })
-      //   .on('mouseover', function (d) {
+      console.log(geoData, 'geoData')
 
-      //     console.log(d, 'd')
-      //   })
+      var heatData = typeof this.heatLayer._heat === 'undefined' ? [] : this.heatLayer._heat._data
+      console.log(heatData, 'thisheatlayer')
 
-      var heat = L.heatLayer(geoData, {
+      var newGeoData = heatData.filter((el) => {
+        return geoData.indexOf(el) === -1
+      })
+
+      console.log(newGeoData, 'newGeoData')
+
+      this.heatLayer = L.heatLayer(geoData, {
         radius: 10,
         blur: 15,
         maxZoom: 8,
         minZoom: 25,
-        // max: 2,
+        id: 'heatmap.population',
         minOpacity: 0.5
       }).addTo(this.map)
 
-      console.log(heat)
+      d3.select('.leaflet-heatmap-layer')
     },
     projectPoint (x, y) {
       var point = this.map.latLngToLayerPoint(new L.LatLng(y, x))
