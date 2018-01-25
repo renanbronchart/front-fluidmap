f97d91ad9710831f4dbd00a074559f11262f4135
renanbronchart <renan.bronchart@gmail.com>
2018-01-25 22:57:31 +0100

    [Feat:Notification] Add molecules component notification

---
+++ src/components/molecules/Notifications.vue
@@ -0,0 +1,24 @@
+<template>
+  <span class="notification">{{count}}</span>
+</template>
+
+<script>
+  export default {
+    props: {
+      count: {
+        type: Number,
+        default: 0
+      }
+    }
+  }
+</script>
+
+<style lang='scss' scoped>
+  @import '~stylesheets/helpers/_variables.scss';
+
+  .notification {
+    background: linear-gradient(to right, $color-cyan, $color-caribbean-green);
+    border-radius: 100%;
+    padding: 5px 10px;
+  }
+</style>
