ebed62a8769635ff4771ca7e017a70920cabbdb4
renanbronchart <renan.bronchart@gmail.com>
2018-02-18 15:33:02 +0100

    [Feat:header] Add style on header

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
-    <header class="header">
-      <span>Vue.js PWA</span>
-    </header>
+    <TheHeader
+      title="JO Paris 2024"
+      @clickAction="clickAction"
+      @clickBrand="clickBrand"
+    />
     <main>
       <router-view></router-view>
     </main>
@@ -15,6 +17,7 @@ import 'stylesheets/style.scss'
 import { mapActions } from 'vuex'
 
 import Modal from '@/components/elements/Modal.vue'
+import TheHeader from '@/components/elements/header/TheHeader.vue'
 
 export default {
   name: 'app',
@@ -24,10 +27,17 @@ export default {
   methods: {
     ...mapActions([
       'getPlaces'
-    ])
+    ]),
+    clickAction () {
+      this.$router.push({name: 'Home'})
+    },
+    clickBrand () {
+      this.$router.push({name: 'Home'})
+    }
   },
   components: {
-    Modal
+    Modal,
+    TheHeader
   }
 }
 </script>
@@ -40,42 +50,8 @@ body {
   background: $color-whisper;
 }
 
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
 main {
   padding: 80px 0 0 0;
   min-height: calc(100vh);
-}
-
-.header {
-  border-bottom: 1px solid $gray-lighter;
-}
-
-header {
-  width: 100%;
-  height: 80px;
-  position: fixed;
-  top: 0;
-  left: 0;
-  background: white;
-  z-index: $z-index-header;
-  margin: 0;
-  padding: 0 16px 0 24px;
-}
-
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
 }
 </style>
